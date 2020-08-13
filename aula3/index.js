2c640c15e54bc7dc166edcaed31658d3089b6965
Jonatan.Knaul <jonatan.knaul@senior.com.br>
2020-08-12 21:20:42 -0300

    no message

---
+++ aula3/index.js
@@ -0,0 +1,10 @@
+console.log("Jonatan 'Knaul'");
+console.log('Jonatan "Knaul"');
+console.log('Jonatan Knaul');
+console.log("Jonatan Knaul");
+
+console.log(123456);
+
+console.log(123456, 123456.88465465);
+
+console.log(35, 15.86, 'Jonatan Knaul') ;
