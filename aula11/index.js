45ebb03262748b15ef79e77c9d216b9d79638653
Jonatan.Knaul <jonatan.knaul@github.com>
2020-08-13 20:10:17 -0300

    up

---
+++ aula11/index.js
@@ -0,0 +1,12 @@
+/**
+ * Artimeticos
+ * + - / *
+ * ** potencia 
+ * % Resto da divisão
+ */
+
+const num1 = 10;
+const num2 = 5;
+console.log(num1 + num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
---
+++ aula9/index.js
@@ -0,0 +1,6 @@
+//ECMAScript 2015 (ES6)
+
+var nome = 'Jonatan';
+var nome = 'knaul';
+
+console.log(nome);
