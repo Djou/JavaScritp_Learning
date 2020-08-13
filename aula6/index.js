5cb5de425b7f175c7cf9a8e884a0c83b5190b32e
Jonatan.Knaul <jonatan.knaul@github.com>
2020-08-12 22:47:10 -0300

    Create index.js

---
+++ aula6/index.js
@@ -0,0 +1,26 @@
+let nome;
+nome = 'Jonatan knaul'
+
+let nome1 = 'Jonatan';
+var nome2 = 'knaul';
+
+console.log(nome1);
+console.log(nome2, 'knaul');
+
+console.log(nome);
+
+nome = 'jonatan tiaraju knaul bosenbecker'
+console.log(nome);
+
+
+//Nao podemos criar variaveis com palavras reservadas
+//let if;
+
+//Não podemos comecar um nome de variavel com número
+//Não podem conter espacos ou tracos
+//Utilzamos camelCase
+//CaseSensitive
+let nomeCliente = 'Luiz';
+let nomecliente = 'Pedro';
+
+console.log(nomeCliente, nomecliente);
