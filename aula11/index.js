c8b7c82591c942d219c0c16955b2b9757be446ac
Jonatan.Knaul <jonatan.knaul@github.com>
2020-08-13 20:25:57 -0300

    Update index.js

--- aula11/index.js
+++ aula11/index.js
@@ -5,8 +5,26 @@
  * % Resto da divisão
  */
 
+//Precedencia da operação
+// () ** * / % + -  
+
 const num1 = 10;
 const num2 = 5;
 console.log(num1 + num2);
 console.log(num1 ** num2);
 console.log(num1 % num2);
+
+
+let contador = 1;
+contador++;
+console.log(contador);
+
+++contador;
+console.log(contador);
+
+--contador;
+console.log(contador++);
+console.log(++contador);
+
+console.log(contador--);
+console.log(--contador);
