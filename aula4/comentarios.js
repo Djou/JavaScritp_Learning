0582395f2d7cb96d50cd0d648d06b6c6cfe455a6
Jonatan.Knaul <jonatan.knaul@senior.com.br>
2020-08-12 22:26:04 -0300

    teste

--- aula4/comentarios.js
+++ aula4/comentarios.js
@@ -3,4 +3,12 @@
 console.log('Linha 1'); //Aqui tem outro comentario
 //console.log('Linha 2');
 console.log('Linha 3');
+//Comentario
 console.log('Linha 4');
+
+/*
+
+Bloco de comentarios
+Linha de comentarios
+
+*/
---
+++ aula5/js/index.js
@@ -0,0 +1,4 @@
+console.log('Oi');
+console.log('TESTE');
+
+alert('teste')
