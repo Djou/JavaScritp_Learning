19d60cce172e7619bfad55276b0faf4107c7e041
Jonatan.Knaul <jonatan.knaul@github.com>
2020-08-13 21:39:54 -0300

    up

--- aula12/js/script.js
+++ aula12/js/script.js
@@ -1,1 +1,32 @@
-alert('Com a nossa mensagem');
+let num1 = parseInt(window.prompt('Digite o primeiro número'));
+let num2 = parseInt(window.prompt('Digite o segundo número'));
+console.log(num1, num2);
+
+let resultado1 = num1 + num2;
+console.log(num1, num2, resultado1);
+
+window.alert('O resultado dos números informados é : ' + resultado1);
+
+
+
+
+
+
+//Viajem
+// //alert('Com a nossa mensagem');
+
+// //window.confirm('TESTE ?');
+
+// let teste;
+// teste = window.prompt('Digite o seu nome');
+
+// window.alert(teste);
+
+// const confirma = window.confirm('O valor digitado é', confirma, '?'); 
+// window.alert(confirma);
+
+// if (confirma) {
+//     window.alert(confirma)
+// } else {
+//     window.alert(confirma)
+// }
