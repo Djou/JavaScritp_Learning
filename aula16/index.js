998ec055d48fc2d6cdb70af724a83d02fd8f6198
Jonatan.Knaul <jonatan.knaul@github.com>
2020-08-16 21:44:53 -0300

    Update index.js

--- aula16/index.js
+++ aula16/index.js
@@ -1,5 +1,34 @@
-let num1 = 1;
-let num2 = 2.5;
+//Padrão IEEE 754-2008
+
+
+let num1 = 10;
+let num2 = 2.5555;
 
 console.log(num1 + num2);
 console.log(num1.toString() + num2.toString());
+
+//Binario
+console.log(num1.toString(2));
+
+//Arredondamento
+console.log(num2.toFixed(2));
+
+console.log(Number.isInteger(num1));
+
+let temp = num1 * 'Olá';
+console.log(temp);
+
+console.log(Number.isNaN(temp));
+
+
+num1 = 0.7;
+num2 = 0.1;
+
+console.log(num1 + num2);
+
+num1 += num2;
+console.log(num1.toFixed(2));
+console.log(Number.isInteger(num1));
+
+num1 = parseFloat(num1.toFixed(2));
+console.log(Number.isInteger(num1));
