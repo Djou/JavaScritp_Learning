bf6b0016ee9012da912456513248ab8b92610857
Jonatan.Knaul <jonatan.knaul@github.com>
2020-08-23 20:13:40 -0300

    Update main.js

--- ExercCompleto/assets/js/main.js
+++ ExercCompleto/assets/js/main.js
@@ -1,32 +1,64 @@
-//1 - Capturar evento do formulario
-//2 - Criar function para setar o resultado
-
-
-
 const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     const inputPeso1 = event.target.querySelector('#peso');
-    const inputPeso1 = event.target.querySelector('#altura');
+    const inputAltura1 = event.target.querySelector('#altura');
 
     const peso = Number(inputPeso1.value);
     const altura = Number(inputAltura1.value);
 
-    console.log(peso, altura);
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+    console.log(imc);
+    console.log(nivelImc);
+    const msg = `Seu IMC é ${imc} (${nivelImc})`;
+    setResultado(msg, true);
 });
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];  
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];   
+    if (imc < 18.5) return nivel[0];
+};
+
+function getImc(peso, altura) {
+    const imc = peso / altura **2;
+    return imc;
+};
 
 function criarP(className) {
     const p = document.createElement('p');
     return p;
 }
 
-
-function setResultado(msg) {
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
     const p = criarP();
-    //resultado.appendChild(p);
+    
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+    
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 }
 
 
