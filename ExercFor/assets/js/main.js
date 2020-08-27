00b1e9d3b1aac7afa6455b8ca51bc2f60967d350
Jonatan.Knaul <jonatan.knaul@github.com>
2020-08-26 23:23:19 -0300

    uo

--- ExercFor/assets/js/main.js
+++ ExercFor/assets/js/main.js
@@ -11,7 +11,14 @@ const div = document.createElement('div');
 for (let i = 0; i < elementos.length; i++) {
     let { tag, texto} = elementos[i];
     let tagCriada = document.createElement(tag);
-    tagCriada.innerHTML = texto;
+    
+    //Certos tambem
+    //tagCriada.innerHTML = texto;
+    //tagCriada.innerText = texto;
+
+    let textoCriado = document.createTextNode(texto);
+    tagCriada.appendChild(textoCriado);
+
     div.appendChild(tagCriada);
 }
 
